c0a4af2f5e729963494408d28a89ceb7346b8172
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-06-15 22:21:52 -0300

    deployment work done and chage the vercel link

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://asset-each-server-site.vercel.app",
 });
 const useAxiosSecure = () => {
   return axiosSecure;
